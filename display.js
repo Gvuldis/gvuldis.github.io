9f1da969aa3a8fd24b1d9ffc074b8f72498f9be9
Gvuldis <gvuldis@gmail.com>
2023-11-23 17:29:29 +0200

    loading bar

--- display.js
+++ display.js
@@ -1,10 +1,13 @@
 window.onload = function() {
     var params = new URLSearchParams(window.location.search);
-    var numberOfBottles = params.get('bottles');
+    var numberOfBottles = parseInt(params.get('bottles'), 10);
     var totalAmount = numberOfBottles * 0.10; // Assuming 10 cents per bottle
 
     document.getElementById('totalAmount').innerText = 'Nodotā summa: ' + totalAmount.toFixed(2) + ' EUR';
 
     var progressPercentage = (numberOfBottles / 50) * 100; // Assuming 50 is the goal
     document.getElementById('progressBar').style.width = progressPercentage + '%';
+
+    
+    document.getElementById('currentBottles').innerText = numberOfBottles;
 };
