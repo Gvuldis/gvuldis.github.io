b51b1958429a7b17849c4294e15f298f9caf8d0f
Gvuldis <gvuldis@gmail.com>
2023-11-23 20:23:14 +0200

    try5

--- display.js
+++ display.js
@@ -22,12 +22,13 @@ window.onload = function() {
     // Create and position the triangle indicator
     var triangle = document.createElement('div');
     triangle.classList.add('triangle');
+    progressBarContainer.appendChild(triangle);
     triangle.style.position = 'absolute';
     triangle.style.top = (progressBar.offsetHeight + 5) + 'px'; // Position just below the progress bar
     triangle.style.left = '50%'; // Start at the center
-    progressBarContainer.appendChild(triangle);
+    
     var triangleOffset = progressBar.offsetLeft + (progressValuePosition - triangle.offsetWidth / 2);
-triangle.style.left = triangleOffset + 'px';
+    triangle.style.left = triangleOffset + 'px';
 
     // Ensure the 'current number of bottles' element doesn't overflow the progress container
     if (progressPercentage > 90) {
