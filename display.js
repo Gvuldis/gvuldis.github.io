e259cfaea4b8f26d1df4b3b608ee96968c714ae3
Gvuldis <gvuldis@gmail.com>
2023-11-23 20:01:19 +0200

    try2 dizaina klumes fix

--- display.js
+++ display.js
@@ -3,30 +3,31 @@ window.onload = function() {
     var numberOfBottles = parseInt(params.get('bottles'), 10);
     var totalAmount = numberOfBottles * 0.10; // Assuming 10 cents per bottle
 
+    // Update the displayed total amount
     document.getElementById('totalAmount').innerText = 'Nodotā summa: ' + totalAmount.toFixed(2) + ' EUR';
 
-    var progressPercentage = (numberOfBottles / 50) * 100; // Assuming 50 is the goal
+    // Calculate and update the progress bar
+    var progressPercentage = Math.min((numberOfBottles / 50) * 100, 100); // Assuming 50 is the goal, max out at 100%
     document.getElementById('progressBar').style.width = progressPercentage + '%';
 
-    
-    document.getElementById('currentBottles').innerText = numberOfBottles;
+    // Create and position the 'current number of bottles' element
     var currentBottlesElement = document.getElementById('currentBottles');
-    currentBottlesElement.style.left = progressPercentage + '%';
-    currentBottlesElement.innerText = numberOfBottles; // Set the current number of bottles
     var progressBar = document.getElementById('progressBar');
-var currentValue = document.createElement('div');
-currentValue.classList.add('current-value');
-currentValue.innerText = '15'; // This should be dynamically set based on the progress
-currentValue.style.left = (progressBar.offsetWidth * (15 / 50) - 10) + 'px'; // 15 is the current value, 50 is the max
+    var progressBarContainer = progressBar.parentElement;
+    var progressContainerWidth = progressBarContainer.offsetWidth;
+    var progressValuePosition = progressPercentage * progressContainerWidth / 100 - currentBottlesElement.offsetWidth / 2;
+    currentBottlesElement.style.left = progressValuePosition + 'px';
+    currentBottlesElement.innerText = numberOfBottles; // Set the current number of bottles
 
-var triangle = document.createElement('div');
-triangle.classList.add('triangle');
-triangle.style.left = (progressBar.offsetWidth * (15 / 50) - 10) + 'px'; // Align with the current value
+    // Create and position the triangle indicator
+    var triangle = document.createElement('div');
+    triangle.classList.add('triangle');
+    triangle.style.position = 'absolute';
+    triangle.style.top = progressBar.offsetTop - 10 + 'px'; // Adjust top position as needed
+    triangle.style.left = progressValuePosition + 'px';
+    progressBarContainer.appendChild(triangle);
 
-// Append to your progress bar container
-progressBar.parentElement.appendChild(currentValue);
-progressBar.parentElement.appendChild(triangle);
-    // Adjust the translateX value if it goes beyond the progress bar boundaries
+    // Ensure the 'current number of bottles' element doesn't overflow the progress container
     if (progressPercentage > 90) {
         currentBottlesElement.style.transform = 'translateX(-100%)';
     } else if (progressPercentage < 10) {
