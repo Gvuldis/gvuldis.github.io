cf46413a3ea40ebf8d34144288abdaebe5b3113d
Gvuldis <gvuldis@gmail.com>
2023-11-23 18:44:37 +0200

    csssss

--- display.js
+++ display.js
@@ -13,7 +13,19 @@ window.onload = function() {
     var currentBottlesElement = document.getElementById('currentBottles');
     currentBottlesElement.style.left = progressPercentage + '%';
     currentBottlesElement.innerText = numberOfBottles; // Set the current number of bottles
+    var progressBar = document.getElementById('progressBar');
+var currentValue = document.createElement('div');
+currentValue.classList.add('current-value');
+currentValue.innerText = '15'; // This should be dynamically set based on the progress
+currentValue.style.left = (progressBar.offsetWidth * (15 / 50) - 10) + 'px'; // 15 is the current value, 50 is the max
 
+var triangle = document.createElement('div');
+triangle.classList.add('triangle');
+triangle.style.left = (progressBar.offsetWidth * (15 / 50) - 10) + 'px'; // Align with the current value
+
+// Append to your progress bar container
+progressBar.parentElement.appendChild(currentValue);
+progressBar.parentElement.appendChild(triangle);
     // Adjust the translateX value if it goes beyond the progress bar boundaries
     if (progressPercentage > 90) {
         currentBottlesElement.style.transform = 'translateX(-100%)';
