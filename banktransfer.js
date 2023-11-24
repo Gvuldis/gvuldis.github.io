eed82e1325909421a470409c3ba333a524fc37d3
Gvuldis <gvuldis@gmail.com>
2023-11-24 10:53:32 +0200

    banktransfer

---
+++ banktransfer.js
@@ -0,0 +1,9 @@
+function transfer(bankName) {
+    // In a real application, you would handle the transfer process here
+    // For this demo, we'll just show an alert
+    alert('Transferring to: ' + bankName);
+
+    // Here you would typically redirect the user or perform an AJAX request
+    // For example:
+    // window.location.href = 'https://www.' + bankName.toLowerCase() + '.lv/transfer';
+}
